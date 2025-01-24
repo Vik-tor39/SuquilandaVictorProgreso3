4d74a9080106a5e0f106d2bbdb09a1bab97672f4
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2025-01-24 14:21:30 -0500

    Definición del tabbar para la navegación entre página de búsqueda y listado de aeropuertos.

--- SuquilandaVictorProgreso3/Page2.xaml.cs
+++ SuquilandaVictorProgreso3/Page2.xaml.cs
@@ -5,5 +5,6 @@ public partial class Page2 : ContentPage
 	public Page2()
 	{
 		InitializeComponent();
-	}
+		BindingContext = new AeropuertoViewModel(DependencyService.Get<IAeropuertoRepository>());
+    }
 }
