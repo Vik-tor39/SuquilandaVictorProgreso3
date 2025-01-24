b19b21b09a0ca4f3e81dc0ed275ec58ac4eba1ce
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2025-01-24 12:22:17 -0500

    Se inicializó el repositorio SQLite en el App Cs

--- SuquilandaVictorProgreso3/App.xaml.cs
+++ SuquilandaVictorProgreso3/App.xaml.cs
@@ -5,7 +5,9 @@
         public App()
         {
             InitializeComponent();
-
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "Aeropuerto.db3");
+            var repo = new AeropuertoRepository(dbPath);
+            DependencyService.RegisterSingleton<IAeropuertoRepository>(repo);
             MainPage = new AppShell();
         }
     }
