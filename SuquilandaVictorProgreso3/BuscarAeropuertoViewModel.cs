088d76a465124cfdaebe81c2926ead0860664e50
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2025-01-24 15:00:26 -0500

    Corrección del ViewModel para la búsqueda de aeropuerto, además se incorporó un try-catch para el control de errores.

--- SuquilandaVictorProgreso3/BuscarAeropuertoViewModel.cs
+++ SuquilandaVictorProgreso3/BuscarAeropuertoViewModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
-using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -29,28 +29,41 @@ namespace SuquilandaVictorProgreso3
         public BuscarAeropuertoViewModel(IAeropuertoRepository repo)
         {
             _repo = repo;
-            SearchCommand = new Command(async () => await searchAeropuerto());
-            ClearCommand = new Command(() => SearchQuery = string.Empty);
+            SearchCommand = new Command(async () => await SearchAeropuerto());
+            ClearCommand = new Command(() =>
+            {
+                SearchQuery = string.Empty;
+                SearchResult = string.Empty;
+            });
         }
-        private async Task searchAeropuerto()
+
+        private async Task SearchAeropuerto()
         {
-            using var client = new HttpClient();
-            var url  = $"https://freetestapi.com/api/v1/airports?search={SearchQuery}";
-            var response = await client.GetFromJsonAsync<List<Arepuerto>>(url);
-            if (response == null && response.Any())
+            try
             {
-                var aeropuerto = response.First();
-                _repo.GuardarArepuerto(aeropuerto, "VSuquilanda");
-                SearchResult = $"Name: {aeropuerto.Name}, " +
-                    $"Country: {aeropuerto.Country}, " +
-                    $"Latitude: {aeropuerto.Latitude}, " +
-                    $"Longitude: {aeropuerto.Longitude}, " +
-                    $"Email: {aeropuerto.email}, " +
-                    $"VSuquilanda: {aeropuerto.VSuquilanda}";
+                using var client = new HttpClient();
+                var url = $"https://freetestapi.com/api/v1/airports?search={SearchQuery}";
+                var response = await client.GetFromJsonAsync<List<Arepuerto>>(url);
+
+                if (response != null && response.Any())
+                {
+                    var aeropuerto = response.First();
+                    _repo.GuardarArepuerto(aeropuerto, "VSuquilanda");
+                    SearchResult = $"Name: {aeropuerto.Name}, " +
+                                   $"Country: {aeropuerto.Country}, " +
+                                   $"Latitude: {aeropuerto.Latitude}, " +
+                                   $"Longitude: {aeropuerto.Longitude}, " +
+                                   $"Email: {aeropuerto.email}, " +
+                                   $"VSuquilanda: {aeropuerto.VSuquilanda}";
+                }
+                else
+                {
+                    SearchResult = "No results found";
+                }
             }
-            else
+            catch (Exception ex)
             {
-                SearchResult = "No results found";
+                SearchResult = $"Error: {ex.Message}";
             }
         }
     }
